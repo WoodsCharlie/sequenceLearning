fd580b4c7879a810878676a6de27da09dc248583
Jack Wheeler <jackwheeler2023@u.northwestern.edu>
2023-12-28 14:10:02 -0600

    updated discardbooard

--- _DiscardBoard.py
+++ _DiscardBoard.py
@@ -1,9 +1,15 @@
+import numpy as np
+
 class DiscardBoard():
 
     def __init__(self):
-        self.discarded = []
+        self.discarded = np.zeros(52, dtype=np.int8)
         
     def add_to_board(self, discarded_card):
-
-        self.discarded.append(discarded_card)
+        self.discarded[discarded_card] += 1
+        if(self.discarded[discarded_card] > 2):
+            raise ValueError('Too many copies of card')
         
+
+    def num_played(self, card_int):
+        return self.discarded[card_int]
